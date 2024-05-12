4ebadc56744187ecdcc13028d5f8b4c029d00152
Mohammad jamshidnia <mohammad_jamshidnia@yahoo.com>
2024-05-13 02:49:27 +0330

    Set AccesToken in Headers With Interceptors

--- src/services/User.js
+++ src/services/User.js
@@ -1,5 +1,4 @@
 import api from "../configs/Api";
-import { getCookie } from "../utils/cookie";
-const token = getCookie("accessToken");
-const getProfile = () => api.get("user/whoami",{headers:{Authorization:`bearer ${token}`}});
+
+const getProfile = () => api.get("user/whoami");
 export { getProfile };
