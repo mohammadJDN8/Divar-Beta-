e97d11039259291957e38fea06ff5f12c645c237
Mohammad jamshidnia <mohammad_jamshidnia@yahoo.com>
2024-05-13 01:50:24 +0330

    ReactQuery Configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import Router from "./Router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import defaultOptions from "./configs/reactQuery";
 
 function App() {
+  const queryClient = new QueryClient({ defaultOptions });
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
---
+++ src/configs/reactQuery.js
@@ -0,0 +1,9 @@
+const defaultOptions = {
+  queries: {
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    staleTime: 60 * 1000,
+    retry: 1,
+  },
+};
+export default defaultOptions;
