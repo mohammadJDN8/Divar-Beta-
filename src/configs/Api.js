8de95d345be43fc11add7bdf1eb84e063fd3aabc
Mohammad jamshidnia <mohammad_jamshidnia@yahoo.com>
2024-05-13 13:15:20 +0330

    Get New Access Token From Refresh Token

--- src/configs/Api.js
+++ src/configs/Api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getCookie } from "../utils/cookie";
+import { getCookie, setCookie } from "../utils/cookie";
+import { getNewToken } from "../services/Token";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -17,6 +18,23 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  }
+);
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const res = await getNewToken();
+      if (!res?.response) return;
+      setCookie(res.response.data);
+      console.log(res);
+      return api(originalRequest);
+    }
   }
 );
 export default api;
